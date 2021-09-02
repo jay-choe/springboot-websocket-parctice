26bb9a9fb108c3ae6140c6740ffc5ac5f4025d8d
jay-choe <jay0764@naver.com>
2021-09-03 03:23:22 +0900

    initial commit

---
+++ src/test/java/com/jay/sockettest/SockettestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay.sockettest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SockettestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
